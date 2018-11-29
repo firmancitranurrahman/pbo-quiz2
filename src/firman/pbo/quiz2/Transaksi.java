1efcca9f366418d805e5387e9c01b9fd3d5acbea
Firman_CN <FirmanCitraNurrahman>
2018-11-29 22:26:30 +0700

    rombak ulang Class

--- src/firman/pbo/quiz2/Transaksi.java
+++ src/firman/pbo/quiz2/Transaksi.java
@@ -15,35 +15,30 @@ public class Transaksi {
     private float total; // atribut yang digunakan untuk menaruh hasil perhitungan
 
     
-    public Transaksi(String kode, ArrayList<Item> items) {
-        this.kode = kode;
-        this.items = items;
+      public Transaksi (String kode , ArrayList<Item> items) {
+        this.kode = kode ;
+        this.items = items ;
+        
     }
 
-    public ArrayList<Item> getItems() {
-        return items;
-    }
-
-    public void setItems(ArrayList<Item> items) {
-        this.items = items;
-    }
+ 
     //setter total 
-    public void setTotal() {
-        for(Item item:this.items){
-            total=0;  
-            total += item.getTotal();
+     public void setTotal () {
+        float total = 0 ;
+        for (Item item : this.items) {
+            total += item.getTotal() ;
         }
-        this.total = total;
+        this.total = total ;
     }
-    public String strukDetail() { 
-        setTotal();
-        String struk = "";
-        struk += "Kode\t\t: "+ this.kode +"\n";
-        struk += "Daftar Belanja : \n";
-        for(Item item : this.items) {
-            struk += "\t"+ item.getNamaBarang() +"(x"+ item.getSatuan() +") : "+ item.getTotal() +"\n";
+    public String Pembayaran() {
+        setTotal () ;
+        String obt = "" ;
+        obt += "Kode\t\t : " + this.kode +"\n" ;
+        obt += "Daftar Belanja : \n" ;
+        for (Item item : this.items) {
+            obt += "\t" + item.getNamaBarang()+ "(x" + item.getJumlah() + ") : " + item.getTotal() + "\n" ;
         }
-        struk += "Total\t\t: "+ this.total;
-        return struk;
+        obt += "Total\t\t : " + this.total ;
+        return obt ;
     }
 }
